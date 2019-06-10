e51384e6553308e7b0e838120551ea6f09c1b4dc
Laura <lmcsay@gmail.com>
2019-06-10 16:59:53 -0500

    html routes added

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+// dependencies
+var path = require('path');
+
+module.exports = function(app) {
+
+    // route to home page
+    app.get('/', function (request, response) {
+        response.sendFile(path.join(_dirname, "../public/home.html"));
+    })
+
+    // route to survey page
+    app.get('/survey', function (request, response) {
+        response.sendFile(path.join(_dirname, "../public/survey.html"));
+    })
+}
