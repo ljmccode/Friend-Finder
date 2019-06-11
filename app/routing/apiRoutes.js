5d957e583c5ecb029e10c9c3a7cd4be868977e0d
Laura <lmcsay@gmail.com>
2019-06-10 19:55:40 -0500

    find best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,5 +13,31 @@ module.exports = function(app) {
         // creating a variable for new user scores
         var newUser = request.body
         var userScores = request.body.scores
+
+        // find match!
+        var matchIndex = '';
+        friendMatch = '';
+
+        // set base differential that will get replaced
+        var totalDifference = 100;
+
+        for (var i=0; i<friends.length; i++) {
+            var currentDiff = 0;
+            for (var j=0; j < userScores.length; j++) {
+                currentDiff += Math.abs(friends[i].scores[j] - userScores[j]);
+            }
+            // if better match, replace until cycled through all friends
+            if(currentDiff < totalDifference) {
+                totalDifference = currentDiff;
+                matchIndex = i;
+                friendMatch = friends[matchIndex]
+                
+            }
+        }
+        // push new user to friends data
+        friends.push(newUser);
+
+        // respond with best match in JSON
+        response.json(friendMatch);
     })
 }
--- server.js
+++ server.js
@@ -7,3 +7,11 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// routing
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("app listening on PORT: " + PORT)
+})
