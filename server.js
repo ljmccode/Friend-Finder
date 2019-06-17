d239b558c8eb03293e41ecbf7e014f58b51f657e
Laura <lmcsay@gmail.com>
2019-06-17 14:52:36 -0500

    Added some CSS

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 var express = require("express");
-var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-// Sets up the Express app to handle data parsing
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json({type: 'application/vnd.api+json'}))
-// app.use(bodyParser.json());
-// app.use(bodyParser.text());
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
-
-
-// // routing
-// apiRoutes(app);
-// htmlRoutes(app);
 
 app.listen(PORT, function() {
     console.log("app listening on PORT: " + PORT)
